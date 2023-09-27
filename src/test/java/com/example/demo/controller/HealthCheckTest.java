1111d6a160a1b5addfde873c0913d838a8785995
sangminee <lsmlee99@gmail.com>
2023-09-27 12:20:16 +0900

    [2-3] userController 테스트 코드 추가

---
+++ src/test/java/com/example/demo/controller/HealthCheckTest.java
@@ -0,0 +1,27 @@
+package com.example.demo.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase
+class HealthCheckTest {
+    // mockMvc : 컨트롤러 테스트를 위해서 사용, API 테스트 하는데 많이 사용되는 도구임
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void 헬스_체크_응답이_200으로_내려온다() throws Exception {
+        mockMvc.perform(get("/health_check.html"))
+                .andExpect(status().isOk());
+    }
+}
