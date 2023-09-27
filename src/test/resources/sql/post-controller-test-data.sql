6e561e0452860d093719f253cfe10809972894e1
sangminee <lsmlee99@gmail.com>
2023-09-27 20:33:09 +0900

    [2-3] postcontoller 및 2개의 컨트롤러에 테스트 코드 추가

---
+++ src/test/resources/sql/post-controller-test-data.sql
@@ -0,0 +1,4 @@
+insert into `users` (`id`, `email`,`nickname`,`address`,`certification_code`,`status`,`last_login_at`)
+values (1,'kok202@naver.com','kok202','Seoul','aaa-aaa-aaa-aaa-aaa','ACTIVE', 0);
+insert into `posts` (`id`,`content`,`created_at`,`modified_at`,`user_id`)
+values (1,'helloworld',13513515,0,1);
