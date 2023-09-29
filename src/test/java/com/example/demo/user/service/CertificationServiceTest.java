3368ba252c7acc664e3c43642cfe1dff0c9ca1f7
sangminee <lsmlee99@gmail.com>
2023-09-29 23:57:23 +0900

    [4-2] CertificationService 테스트 추가

---
+++ src/test/java/com/example/demo/user/service/CertificationServiceTest.java
@@ -0,0 +1,25 @@
+package com.example.demo.user.service;
+
+import com.example.demo.mock.FakeMailSender;
+import com.example.demo.user.service.port.MailSender;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CertificationServiceTest {
+
+    @Test
+    public void 이메일과_컨텐츠가_제대로_만들어져서_보내지는지_테스트한다(){
+        // given
+        FakeMailSender fakeMailSender = new FakeMailSender();
+        CertificationService certificationService = new CertificationService(fakeMailSender);
+
+        // when
+        certificationService.send("kok202@naver.com",1, "aaa-aaa-aaa-aaa-aaa");
+
+        // then
+        assertThat(fakeMailSender.email).isEqualTo("kok202@naver.com");
+        assertThat(fakeMailSender.title).isEqualTo("Please certify your email address");
+        assertThat(fakeMailSender.content).isEqualTo("Please click the following link to certify your email address: http://localhost:8080/api/users/1/verify?certificationCode=aaa-aaa-aaa-aaa-aaa");
+    }
+}
