750b3b7819206c91d9416c60c25d796ecbd328b7
sangminee <lsmlee99@gmail.com>
2023-10-05 19:08:34 +0900

    [4-7] 리팩토링

--- src/test/java/com/example/demo/mock/TestContainer.java
+++ src/test/java/com/example/demo/mock/TestContainer.java
@@ -9,7 +9,7 @@ import com.example.demo.post.service.PostServiceImpl;
 import com.example.demo.post.service.port.PostRepository;
 import com.example.demo.user.controller.UserController;
 import com.example.demo.user.controller.UserCreateController;
-import com.example.demo.user.controller.port.*;
+import com.example.demo.user.controller.port.UserService;
 import com.example.demo.user.service.CertificationService;
 import com.example.demo.user.service.UserServiceImpl;
 import com.example.demo.user.service.port.MailSender;
@@ -21,10 +21,7 @@ public class TestContainer { // Spring의 IoC를 흉내냄
     public final MailSender mailSender;
     public final UserRepository userRepository;
     public final PostRepository postRepository;
-    public final UserReadService userReadService;
-    public final UserCreateService userCreateService;
-    public final UserUpdateService userUpdateService;
-    public final AuthenticationService authenticationService;
+    public final UserService userService;
     public final PostService postService;
     public final CertificationService certificationService;
     public final UserController userController;
@@ -51,18 +48,12 @@ public class TestContainer { // Spring의 IoC를 흉내냄
                 .certificationService(this.certificationService)
                 .build();
 
-        this.userReadService = userService;
-        this.userCreateService = userService;
-        this.userUpdateService= userService;
-        this.authenticationService = userService;
+        this.userService = userService;
         this.userController = UserController.builder()
-                .userReadService(userReadService)
-                .userCreateService(userCreateService)
-                .userUpdateService(userUpdateService)
-                .authenticationService(authenticationService)
+                .userService(userService)
                 .build();
         this.userCreateController = UserCreateController.builder()
-                .userCreateService(userCreateService)
+                .userService(userService)
                 .build();
 
         this.postController = PostController.builder()
